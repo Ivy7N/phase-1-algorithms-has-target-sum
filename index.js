263c0bc637fee533ee2776042793bed105fb14ac
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-10 20:52:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+function hasTargetSum(arr, target) {
+  const seen = new Set();
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+  for (const num of arr) {
+    const complement = target - num;
 
-/* 
-  Add your pseudocode here
-*/
+    if (seen.has(complement)) {
+      return true;
+    }
 
-/*
-  Add written explanation of your solution here
-*/
+    seen.add(num);
+  }
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  return false;
 }
 
 module.exports = hasTargetSum;
+
